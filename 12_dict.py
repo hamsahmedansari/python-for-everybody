365d29fb78229601716612e094f512b30e88291e
Hams A <hams.ahmed.ansari95@gmail.com>
2026-06-05 10:31:39 +0500

    Improve elevator floor selection error handling and add user feedback for invalid inputs.

---
+++ 12_dict.py
@@ -0,0 +1,25 @@
+# 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages
+# The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail
+# The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file
+# After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+counts = dict()
+
+for line in handle:
+    if not line.startswith("From "):
+        continue
+    words = line.split()
+    counts[words[1]] = counts.get(words[1], 0) + 1
+print(counts)
+
+max_count = None
+max_email = None
+for email, count in counts.items():
+    if max_count is None or count > max_count:
+        max_count = count
+        max_email = email
+print(max_email, max_count)
